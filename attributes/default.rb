interviewstreet/languages

commit 191ef3f79781448e599f02d30cd98a0fafeec850

Install java9, update node version to 8.10.0
java9 is now installed in recipes/default.rb and nodejs version is
updated to 8.10.0 (latest LTS version)

Signed-off-by: Rudra Nil Basu <rudra@hackerrank.com>

attributes/default.rb
@@ -45,6 +45,8 @@ default['go']['packages'] = ['encoding/json', 'encoding/csv', 'encoding/xml', 's
 
 # NodeJS
 default['nodejs']['install_method'] = 'binary'
+default['nodejs']['version'] = '8.10.0'
+dedfault['nodejs']['checksum'] = '92220638d661a43bd0fee2bf478cb283ead6524f231aabccf14c549ebc2bc338'
 default['nodejs']['npm_packages'] = [{'name' => 'coffee-script'}, {'name' => 'typescript'},
 	{'name' => 'underscore'}, {'name' => 'lodash'}, {'name' => 'jquery'}, {'name' => 'bignumber.js'}]
 

recipes/default.rb
@@ -71,6 +71,15 @@ java_ark "install-jdk-8" do
     bin_cmds ["java", "javac"]
     action :install
 end
+
+## Install JAVA 9
+java_ark "install-jdk-9" do
+    url node[:java9][:url]
+    app_home node[:java9][:home]
+    bin_cmds ["java", "javac"]
+    action :install
+end
+
 execute 'set-java-home' do
   user 'root'
   command <<-EOH

commit de96b2cbe74a66d814f0dd3390871f4612557ee2

attributes: Correct "default" spelling error
Introduced in:

    191ef3f7: Install java9, update node version to 8.10.0

Signed-off-by: Rudra Nil Basu <rudra@hackerrank.com>

attributes/default.rb
@@ -46,7 +46,7 @@ default['go']['packages'] = ['encoding/json', 'encoding/csv', 'encoding/xml', 's
 # NodeJS
 default['nodejs']['install_method'] = 'binary'
 default['nodejs']['version'] = '8.10.0'
-dedfault['nodejs']['checksum'] = '92220638d661a43bd0fee2bf478cb283ead6524f231aabccf14c549ebc2bc338'
+default['nodejs']['checksum'] = '92220638d661a43bd0fee2bf478cb283ead6524f231aabccf14c549ebc2bc338'
 default['nodejs']['npm_packages'] = [{'name' => 'coffee-script'}, {'name' => 'typescript'},
 	{'name' => 'underscore'}, {'name' => 'lodash'}, {'name' => 'jquery'}, {'name' => 'bignumber.js'}]
 
de96b2cbe74a66d814f0dd3390871f4612557ee2
Rudra Nil Basu <rudra@hackerrank.com>
2018-03-12 17:23:59 +0530

    attributes: Correct "default" spelling error
    Introduced in:
    
        191ef3f7: Install java9, update node version to 8.10.0
    
    Signed-off-by: Rudra Nil Basu <rudra@hackerrank.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -46,7 +46,7 @@ default['go']['packages'] = ['encoding/json', 'encoding/csv', 'encoding/xml', 's
 # NodeJS
 default['nodejs']['install_method'] = 'binary'
 default['nodejs']['version'] = '8.10.0'
-dedfault['nodejs']['checksum'] = '92220638d661a43bd0fee2bf478cb283ead6524f231aabccf14c549ebc2bc338'
+default['nodejs']['checksum'] = '92220638d661a43bd0fee2bf478cb283ead6524f231aabccf14c549ebc2bc338'
 default['nodejs']['npm_packages'] = [{'name' => 'coffee-script'}, {'name' => 'typescript'},
 	{'name' => 'underscore'}, {'name' => 'lodash'}, {'name' => 'jquery'}, {'name' => 'bignumber.js'}]
 
